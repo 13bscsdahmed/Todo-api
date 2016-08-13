99b8db2659439c331bddebd40718f3fd2b7792de
Danish Ahmed <13bscsdahmed@seecs.edu.pk>
2016-08-13 18:05:47 +0500

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -1,11 +1,64 @@
-var express= require('express');
-var app= express();
-var PORT= process.env.PORT || 3000;   // process.env.PORT is environment variable provided by heroku. If app runs on heroku, it uses this variable for port number, otherwise uses the other port
+var express=require('express');
+var app=express();
+var PORT= process.env.port|| 3000;
 
-app.get('/', function(req,res){
-	res.send('Todo API Root')
+
+var todos=[{
+	id: 1,
+	description: 'Meet mom for lunch',
+	completed: false
+},
+{
+	id:2,
+	description: 'Go to market',
+	completed: false
+
+},
+{
+	id:3,
+	description: 'Play Tennis',
+	completed: true
+}];
+
+
+app.get('/',function(req,res){
+	res.send('TODO API Root');
 });
 
-app.listen(PORT, function(){
-	console.log('Express listening on port'+PORT+'!');
+app.get('/todos',function(req,res){
+	res.json(todos);
+});
+
+app.get('/todos/:id', function(req,res){
+	var todoID= parseInt(req.params.id,10);
+	var matchdedtodo;
+	//res.send('Asking for todo with id of:'+req.params.id);
+	//res.send(todos.length);
+	todos.forEach(function(todo){
+		if(todoID === todo.id) {
+
+
+			matchdedtodo= todo;
+
+		}
+			
+	});
+
+	if(matchdedtodo){
+
+		res.json(matchdedtodo);
+
+	}
+	else{
+		res.status(404).send();
+	}
+	
+
+});
+		
+
+
+
+app.listen(PORT,function(){
+	console.log('Express listening on PORT'+ PORT+'!');
 });
