2e07b4258eba3d255120adff6ee00eaaf471a976
Danish Ahmed <13bscsdahmed@seecs.edu.pk>
2016-08-15 01:16:59 +0500

    Refactor API with _

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express=require('express');
 var app=express();
 var PORT= process.env.PORT || 3000;
 var bodyParser= require('body-parser');
+var _= require('underscore');
 
 var todos=[];
 var todoNextId= 1;
@@ -17,18 +18,19 @@ app.get('/todos',function(req,res){
 
 app.get('/todos/:id', function(req,res){
 	var todoID= parseInt(req.params.id,10);
-	var matchdedtodo;
+	var matchdedtodo= _.findWhere(todos,{id: todoID}); //underscore library
+	//var matchdedtodo;
 	//res.send('Asking for todo with id of:'+req.params.id);
 	//res.send(todos.length);
-	todos.forEach(function(todo){
-		if(todoID === todo.id) {
+	// todos.forEach(function(todo){
+	// 	if(todoID === todo.id) {
 
 
-			matchdedtodo= todo;
+	// 		matchdedtodo= todo;
 
-		}
+	// 	}
 			
-	});
+	// });
 
 	if(matchdedtodo){
 
@@ -45,7 +47,17 @@ app.get('/todos/:id', function(req,res){
 
 
 app.post('/todos', function (req,res){
-	var body= req.body;
+	var body= _.pick(req.body,'description','completed');
+
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length===0){
+
+
+		return res.status(400).send();
+	}
+
+
+	body.description=body.description.trim();
 	body.id=todoNextId;
 	todoNextId+=1
 	//console.log('description:'+body.description);
