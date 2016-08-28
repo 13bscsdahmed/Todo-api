bb7459c3ce3eacbd6cf44bb10f29a89392469f32
Danish Ahmed <13bscsdahmed@seecs.edu.pk>
2016-08-29 01:51:52 +0500

    Add Postgress for production

--- db.js
+++ db.js
@@ -1,8 +1,20 @@
 var Sequelize= require('sequelize');
-var sequelize= new Sequelize(undefined, undefined,undefined,{
+var env= process.env.NODE_ENV || 'development';
+var sequelize;
+
+if(env==='production'){  	//this will be true when only running on heroku
+	sequelize= new Sequelize(process.env.DATABASE_URL,{
+		dialect:'postgres'
+
+	});
+
+}
+else{
+	var sequelize= new Sequelize(undefined, undefined,undefined,{
 	'dialect': 'sqlite',
 	'storage': __dirname+ '/Data/dev-todo-api.sqlite'
-});
+	});
+}
 
 var db={};
 db.todo=sequelize.import(__dirname+'/models/todo.js');
