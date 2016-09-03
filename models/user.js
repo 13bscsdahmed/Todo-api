a7153b6bce060115ec8d9ca9f8ca87c05233a1f9
Danish Ahmed <13bscsdahmed@seecs.edu.pk>
2016-09-03 20:51:21 +0500

    Convert email to lower case

---
+++ models/user.js
@@ -0,0 +1,33 @@
+module.exports= function(sequelize,DataTypes){
+	return sequelize.define('user',{
+	email:{
+		type: DataTypes.STRING,
+		allowNull:false,
+		unique: true,		//allows unique emails,. no 2 same emails
+		validate:{
+			isEmail:true
+		}
+	},
+	password:{
+		type: DataTypes.STRING,
+		allowNull:false,
+		validate: {
+			len: [7,100]
+			}
+		}
+
+	},
+	{
+		hooks:{
+			beforeValidate: function(user,options){
+				if(typeof user.email==='string'){
+					user.email=user.email.toLowerCase();
+				}
+
+			}
+		}
+	
+	
+	});
+
+};
